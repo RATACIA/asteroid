c4f36f1a6ac81d085d2a9af6ece09b12e6e19fe9
RATACIA <sebastian.rat.99@gmail.com>
2025-01-22 13:50:48 +0200

    added sprites and player model in progress

--- main.py
+++ main.py
@@ -2,17 +2,19 @@ import pygame
 from constants import *
 
 def main():
-    print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
-
     pygame.init()
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            return
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
     while True:
+        for event in pygame.event.get():
+            dt = clock.tick(60)/1000
+            if event.type == pygame.QUIT:
+                return
+            
         screen.fill((0,0,0))
         pygame.display.flip()
+
+
 if __name__ == "__main__":
     main()
